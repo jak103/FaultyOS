9823da3d5eed7f46c446e345f172b4df155a8975
Jacob Christensen <jchristensen@usurf.usu.edu>
2019-06-11 18:14:40 -0600

    first

---
+++ FaultyOS/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace FaultyOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.commandBox = new System.Windows.Forms.TextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.killRateBox = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.killButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.killRateBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // commandBox
+            // 
+            this.commandBox.Location = new System.Drawing.Point(101, 424);
+            this.commandBox.Name = "commandBox";
+            this.commandBox.Size = new System.Drawing.Size(695, 26);
+            this.commandBox.TabIndex = 0;
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(631, 456);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(75, 38);
+            this.runButton.TabIndex = 1;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(12, 12);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(784, 371);
+            this.outputBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 427);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Process:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 473);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Kill Rate:";
+            // 
+            // killRateBox
+            // 
+            this.killRateBox.Location = new System.Drawing.Point(101, 471);
+            this.killRateBox.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.killRateBox.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.killRateBox.Name = "killRateBox";
+            this.killRateBox.Size = new System.Drawing.Size(75, 26);
+            this.killRateBox.TabIndex = 5;
+            this.killRateBox.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.killRateBox.ValueChanged += new System.EventHandler(this.killRateBox_ValueChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 390);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(138, 24);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Enable Output";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // killButton
+            // 
+            this.killButton.Enabled = false;
+            this.killButton.Location = new System.Drawing.Point(721, 456);
+            this.killButton.Name = "killButton";
+            this.killButton.Size = new System.Drawing.Size(75, 38);
+            this.killButton.TabIndex = 1;
+            this.killButton.Text = "Kill All";
+            this.killButton.UseVisualStyleBackColor = true;
+            this.killButton.Click += new System.EventHandler(this.killButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 511);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.killRateBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.killButton);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.commandBox);
+            this.Name = "Form1";
+            this.Text = "FaultyOS";
+            ((System.ComponentModel.ISupportInitialize)(this.killRateBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox commandBox;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown killRateBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button killButton;
+    }
+}
+
---
+++ FaultyOS/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace FaultyOS
+{
+    public partial class Form1 : Form
+    {
+        private List<Process> processes = new List<Process>();
+        
+        private System.Timers.Timer timer = new System.Timers.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = (double)killRateBox.Value;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            lock (processes)
+            {
+                if (processes.Count > 0)
+                {
+                    List<Process> children = new List<Process>();
+                    foreach (Process p in processes)
+                    {
+                        List<Process> temp = GetChildProcesses(p);
+                        children.AddRange(temp);                        
+                    }
+
+                    processes.AddRange(children);
+
+                    Random rand = new Random();
+
+                    int index = rand.Next(0, processes.Count);
+
+                    Process deadProcess = processes[index];
+
+                    processes.RemoveAt(index);
+                    if (!deadProcess.HasExited)
+                    {
+                        PrintText("Killing process: " + deadProcess.ProcessName + " (" + deadProcess.Id + ")");
+                        deadProcess.Kill();
+                    }
+                }
+            }
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            string[] command = commandBox.Text.Split(' ');
+
+            lock (processes)
+            {
+                timer.Stop();
+                Process p1 = new Process();
+                p1.StartInfo.FileName = command[0];
+
+                if (command.Length > 1)
+                {
+                    p1.StartInfo.Arguments = command[1];
+                }
+
+                p1.Start();
+
+                processes.Add(p1);
+                PrintText(string.Format("Running process: {0}", p1.ProcessName));
+
+                timer.Start();
+            }
+        }
+
+        private void killButton_Click(object sender, EventArgs e)
+        {
+            lock (processes)
+            {
+                timer.Stop();
+                foreach (Process p in processes)
+                {
+                    p.Kill();
+                }
+
+                processes.Clear();
+
+            } 
+        }
+
+        private delegate void StringDelegate(string text);
+
+        private void PrintText(string text)
+        {
+            if (checkBox1.Enabled)
+            {
+                if (outputBox.InvokeRequired)
+                {
+                    outputBox.Invoke(new StringDelegate(PrintText), new object[] { text });
+                }
+                else
+                {
+                    outputBox.AppendText(text + "\r\n");
+                }
+            }
+        }
+
+        
+        public List<Process> GetChildProcesses(Process process)
+        {
+            List<Process> children = new List<Process>();
+            ManagementObjectSearcher mos = new ManagementObjectSearcher(String.Format("Select * From Win32_Process Where ParentProcessID={0}", process.Id));
+
+            foreach (ManagementObject mo in mos.Get())
+            {
+                children.Add(Process.GetProcessById(Convert.ToInt32(mo["ProcessID"])));
+            }
+
+            return children;
+        }
+
+        private void killRateBox_ValueChanged(object sender, EventArgs e)
+        {
+            timer.Stop();
+            timer.Interval = (double)killRateBox.Value;
+            timer.Start();
+        }
+    }
+}
